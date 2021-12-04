a071aead82d91ad33e22c08969097913fb95acdb
SlavikYur <mr.sluvik822@gmail.com>
2021-12-05 00:05:52 +0200

    Added window for height adjustment

---
+++ HeightAdjust.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Table
+{
+    /// <summary>
+    /// Логика взаимодействия для HeightAdjust.xaml
+    /// </summary>
+    public partial class HeightAdjust : Window
+    {
+        public HeightAdjust()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            this.DragMove();
+        }
+
+        #region
+        private void ApplyChanges(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+    }
+}
